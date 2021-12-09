793353b787e763e95ad9596fb429606755be973a
Bruno Cauet <brunocauet@gmail.com>
2021-12-09 11:59:11 +0100

    Day 9 cleanup.

--- 09.lisp
+++ 09.lisp
@@ -10,16 +10,21 @@
 
 (defparameter *test-input* (parse-input (uiop:read-file-lines "09.input.test")))
 
+(defun neighbours (input i j)
+  (remove nil
+	  (list
+	   ;; above
+	   (if (> i 0) (aref input (1- i) j))
+	   ;; left
+	   (if (> j 0) (aref input i (1- j)))
+	   ;; below
+	   (if (< i (1- (array-dimension input 0))) (aref input (1+ i) j))
+	   ;; right
+	   (if (< j (1- (array-dimension input 1))) (aref input i (1+ j))))))
+
 (defun neighbours-are-higher (input i j)
-  (and
-   ;; above
-   (or (<= i 0) (< (aref input i j) (aref input (1- i) j)))
-   ;; left
-   (or (<= j 0) (< (aref input i j) (aref input i (1- j))))
-   ;; below
-   (or (>= i (1- (array-dimension input 0))) (< (aref input i j) (aref input (1+ i) j)))
-   ;; right
-   (or (>= j (1- (array-dimension input 1))) (< (aref input i j) (aref input i (1+ j))))))
+  (every (lambda (neighbour) (< (aref input i j) neighbour))
+	 (neighbours input i j)))
 
 (defun find-low-points (input)
   (loop for i from 0 below (array-dimension input 0)
@@ -40,15 +45,14 @@
      (< -1 j (second dims))
      (< (aref heights i j) 9))))
 
-  
 (defun add-basin-neighbour (heights basins low-point current-x current-y)
   (setf (aref basins current-x current-y) low-point)
   (loop for (delta-x delta-y) in '((-1 0) (1 0) (0 -1) (0 1))
-	if (and (is-valid heights (+ current-x delta-x) (+ current-y delta-y))
-		(not (aref basins (+ current-x delta-x) (+ current-y delta-y))))
-	  do (add-basin-neighbour heights basins low-point
-				  (+ current-x delta-x)
-				  (+ current-y delta-y))))
+	for x2 = (+ current-x delta-x)
+	for y2 = (+ current-y delta-y)
+	if (and (is-valid heights x2 y2)
+		(not (aref basins x2 y2)))
+	  do (add-basin-neighbour heights basins low-point x2 y2)))
 
 (defun build-basins (input low-points)
   "Builds an array of the same size as input where each value is the coords of a low point."
@@ -56,7 +60,8 @@
     ;; for each low point: start from the low point, make all its neighbours point to it,
     ;; then their neighbours
     (loop for low-point in low-points
-	  do (add-basin-neighbour input basins low-point (car low-point) (cdr low-point)))
+	  for (low-point-x . low-point-y) = low-point
+	  do (add-basin-neighbour input basins low-point low-point-x low-point-y))
     basins))
 
 (defun basins-sizes (basins)
@@ -64,15 +69,14 @@
     (loop for x below (array-dimension basins 0) do
       (loop for y below (array-dimension basins 1) do
 	(incf (gethash (aref basins x y) sizes 0))))
-	sizes))
+    sizes))
 
 (defun largest-basins-sizes (basins-hash-table)
   (subseq 
    (sort
     (loop for key being the hash-keys of basins-hash-table
 	    using (hash-value value)
-	  if key
-	  collect value)
+	  if key collect value)
     #'>)
    0 3))
 
@@ -81,4 +85,4 @@
 	      (basins-sizes
 	       (build-basins input (find-low-points input))))))
 
-(print part2 *input*)
+(print (part2 *input*))
