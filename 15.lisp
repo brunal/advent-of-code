738b859468230b9fe49f1ab3297209f79a704e21
Bruno Cauet <brunocauet@gmail.com>
2021-12-15 15:02:41 +0100

    Complete day 15.
    * fix bugs.
    * use a priority queue to solve part2 in reasonable time.

--- 15.lisp
+++ 15.lisp
@@ -1,8 +1,8 @@
+(ql:quickload 'priority-queue)
 
 (defun parse-input (input)
   (let ((array (make-array
-		(list (length input) (length (first input)))
-		:initial-element nil)))
+		(list (length input) (length (first input))))))
     (loop for line in input and i from 0
 	  do (loop for item in (coerce line 'list) and j from 0
 		   do (setf (aref array i j) (digit-char-p item))))
@@ -11,47 +11,75 @@
 (defparameter *input* (parse-input (uiop:read-file-lines "15.input")))
 (defparameter *input-test* (parse-input (uiop:read-file-lines "15.input.test")))
 
-
-;; Let's do a Dijkstra where each cell is a vertex, with up to 4 edges. Those 4
-;; edges have the same weight: the "risk level" of the cell.
-(defun visit (table best-so-far unvisited i j)
-  (loop for (di dj) in '((-1 0) (1 0) (0 -1) (0 1))
+(defun update-neighbours (table best-so-far unvisited me)
+  "Update (i j)'s neighbours weigths in best-so-far and unvisited."
+  (loop with (i . j) = me
+	for (di dj) in '((-1 0) (1 0) (0 -1) (0 1))
 	for ii = (+ i di)
 	for jj = (+ j dj)
-	;; update the neighbours' weights
-	if (member (cons ii jj) unvisited)
-	  do (setf (aref best-so-far ii jj)
-		   (min
-		    (aref best-so-far ii jj)
-		    (+ (aref best-so-far i j) (aref table ii jj)))))
-  ;; mark the current node visited
-  (delete (cons i j) unvisited))
-
-(defun next-to-visit (best-so-far-table unvisited)
-  "Returns the (i . j) in unvisited that minimizes the distance."
-  (loop with lowest-value = most-positive-fixnum
-	with best-so-far = nil
-	for unv in unvisited for (unv-x . unv-y) = unv
-	for value = (aref best-so-far-table unv-x unv-y)
-	if (< value lowest-value)
-	  do (setf best-so-far unv)
-	finally (return best-so-far)))
+	if (and (< -1 ii (array-dimension best-so-far 0))
+		(< -1 jj (array-dimension best-so-far 1)))
+	  do (let ((suggested-value
+		     (+ (aref best-so-far i j) (aref table ii jj))))
+	       ;; update the neighbour's weight
+	       (when (< suggested-value (aref best-so-far ii jj))
+		 (setf (aref best-so-far ii jj) suggested-value)
+		 (priority-queue:pqueue-push
+		  (cons ii jj)
+		  suggested-value
+		  unvisited)))))
 
 (defun shortest-path (table best-so-far unvisited current end)
-  (let ((new-unvisited (visit table best-so-far unvisited (car current) (cdr current))))
-    (unless (equal current end)
-      (shortest-path table best-so-far new-unvisited (next-to-visit best-so-far new-unvisited) end))))
-
+  "Recursively visits from current until end."
+  (update-neighbours table best-so-far unvisited current)
+  (unless (equal current end)
+    (shortest-path table best-so-far unvisited
+		   (priority-queue:pqueue-pop unvisited) end)))
 
 (defun shortest-path-from-start-to-end (table)
   (let* ((best-so-far
-	   (make-array (array-dimensions table) :initial-element most-positive-fixnum))
+	   (make-array (array-dimensions table)
+		       :initial-element most-positive-fixnum))
 	 (max-i (1- (array-dimension table 0)))
 	 (max-j (1- (array-dimension table 1)))
-	 (unvisited
-	   (loop for i from 0 upto max-i
-		 nconc (loop for j from 0 upto max-j
-			     collect (cons i j)))))
+	 (unvisited (priority-queue:make-pqueue #'<)))
     (setf (aref best-so-far 0 0) 0)
-    (shortest-path table best-so-far unvisited '(0 . 0) (cons max-i max-j))
+    (loop for i from 0 upto max-i
+	  do (loop for j from 0 upto max-j
+		   do (priority-queue:pqueue-push
+		       (cons i j)
+		       (aref best-so-far i j)
+		       unvisited)))
+    (shortest-path table best-so-far unvisited '(0 . 0) `(,max-i . ,max-j))
     (aref best-so-far max-i max-j)))
+
+(defun part1 (&optional (input *input*))
+  (shortest-path-from-start-to-end input))
+(assert (= (part1 *input-test*) 40))
+
+(print (part1))
+
+(defun replicate-map (table times)
+  (let* ((max-i (array-dimension table 0))
+	 (max-j (array-dimension table 1))
+	 (new-table (make-array
+		     (mapcar (lambda (n) (* times n))
+			     (array-dimensions table))))
+	 (loop-after-9 (lambda (n) (1+ (mod (1- n) 9)))))
+    (loop for i from 0 below max-i do
+      (loop for j from 0 below max-j do
+	(dotimes (horizontal times)
+	  (dotimes (vertical times)
+	    (setf
+	     (aref new-table
+		   (+ i (* max-i horizontal))
+		   (+ j (* max-j vertical)))
+	     (funcall loop-after-9
+		      (+ horizontal vertical (aref table i j))))))))
+    new-table))
+
+(defun part2 (&optional (input *input*))
+  (part1 (replicate-map input 5)))
+(assert (= (part2 *input-test*) 315))
+
+(print (part2))
