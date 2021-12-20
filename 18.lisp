2f2270ac2dc581804d36135bfcf7a3bd0795367f
Bruno Cauet <brunocauet@gmail.com>
2021-12-20 09:02:09 +0100

    Fail at days 18 and 19...

---
+++ 18.lisp
@@ -0,0 +1,380 @@
+(ql:quickload 'cl-ppcre)
+(ql:quickload 'cl-utilities)
+
+(setf *print-circle* t)
+(defparameter *debug* nil)
+
+(defun snailfish-parse (input)
+  "Turns a string into a snailfish."
+  (with-input-from-string
+      (stream (cl-ppcre:regex-replace-all
+	       ","
+	       (cl-ppcre:regex-replace-all
+		"\\]"
+		(cl-ppcre:regex-replace-all "\\[" input "(")
+		")")
+	       " "))
+    (read stream)))
+    ;; (let ((s (parse-snailfish-structure (read stream))))
+    ;;   (weave-leaves (get-all-leaves s))
+    ;;   s)))
+
+;; reboot :-|
+(defun try-split (s)
+  (if (numberp s)
+      (if (>= s 10)
+	  ;; split the value
+	  (list* t (floor (/ s 2)) (ceiling (/ s 2)) t)
+	  (cons nil t))
+      (destructuring-bind (modified . new-car-s) (try-split (car s))
+	(if modified
+	    (list* t new-car-s (cdr s))
+	    (destructuring-bind (new-cdr-s . modified) (try-split (cdr s))
+	      (list* modified (car s) new-cdr-s))))))
+
+(defun add-leftmost (s value)
+  (if s
+      (if (numberp s)
+	  (+ s value)
+	  (cons (add-leftmost (first s) value) (second s)))))
+
+(defun add-rightmost (s value)
+  (if s
+      (if (numberp s)
+	  (+ s value)
+	  (cons (first s) (add-rightmost (second s) value)))))
+
+(defun try-explode (s &optional (depth 0))
+  (if (numberp s)
+      (list nil s nil nil)
+      (if (>= depth 4)
+	  ;; explode the pair
+	  (progn
+	    (format t "exploding ~a~%" s)
+	    (list t 0 (first s) (second s)))
+	  (destructuring-bind (modified left-reduce left-explode-value right-explode-value)
+	      (try-explode (first s) (1+ depth))
+	    (if modified
+		(list t
+		      (cons left-reduce (add-leftmost (second s) right-explode-value))
+		      left-explode-value
+		      nil)
+		(destructuring-bind (modified right-reduce left-explode-value right-explode-value)
+		    (try-explode (second s) (1+ depth))
+		  (format t "checking if my second of ~a explodes: ~a~%" s (second s))
+		  (if modified
+		      (list t
+			    (cons (add-rightmost (first s) left-explode-value) right-reduce)
+			    nil
+			    right-explode-value)
+		      (list nil s nil nil))))))))
+
+(defun loop-reduce (s)
+  (destructuring-bind (new-s modified) (try-explode s)
+    (if modified
+	(loop-reduce new-s)
+	(destructuring-bind (new-s . modified) (try-split s)
+	  (if modified
+	      (loop-reduce new-s)
+	      new-s)))))
+
+(defun test ()
+  (loop-reduce (snailfish-parse "[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]")))
+;; reboot :-| end
+
+(defun snailfish-print-to-stream (sn st)
+  (ecase (getf sn :type)
+    (pair
+     (format st "[")
+     (snailfish-print-to-stream (getf sn :left) st)
+     (format st ",")
+     (snailfish-print-to-stream (getf sn :right) st)
+     (format st "]"))
+    (leaf (format st "~a" (getf sn :value)))))
+
+(defun snailfish-string (s)
+  "Turns a snailfish into a string."
+  (let ((stream (make-string-output-stream)))
+    (snailfish-print-to-stream s stream)
+    (get-output-stream-string stream)))
+
+(defun parse-snailfish-structure (s &optional (depth 0))
+  "Recursively builds a snailfish from int-or-myself recursive cons cells."
+  ;; note: having all properties on both types means that when we modify either (explode or split)
+  ;; we actually modify the plist rather that than get another list that we would have to assign back.
+  (if (numberp s)
+      (list :type 'leaf
+	    :depth depth
+	    :value s
+	    :prev nil
+	    :next nil)
+      (list :type 'pair
+	    :depth depth
+	    :left (parse-snailfish-structure (first s) (1+ depth))
+	    :right (parse-snailfish-structure (second s) (1+ depth)))))
+
+(defun get-all-leaves (s)
+  "Returns a list of all the leaves in the snailfish."
+  (if (eq 'pair (getf s :type))
+      (nconc (get-all-leaves (getf s :left))
+	     (get-all-leaves (getf s :right)))
+      (list s)))
+
+(defun weave-leaves (leaves)
+  "Sets the :prev and the :next on all leaves."
+  (loop for (prev current next) on (cons nil leaves)
+	while current
+	do (progn
+	     (setf (getf current :prev) prev)
+	     (setf (getf current :next) next))))
+
+;; since we destructively modify the expressions, always return fresh ones.
+(defun input () (mapcar #'snailfish-parse (uiop:read-file-lines "18.input")))
+(defun input-test () (mapcar #'snailfish-parse (uiop:read-file-lines "18.input.test")))
+(defun input-test2 () (mapcar #'snailfish-parse (uiop:read-file-lines "18.input.test2")))
+(defun snailfish-test ()
+  (snailfish-parse "[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]"))
+
+;; note: (setf (getf plist ...) ...) does not always modify in-place the plist (e.g. if
+;; the requested property is missing or has null value.
+;; set-property guarantees the modification is done in-place.
+(defun set-property (s prop value)
+  "Sets the property to value in s in-place."
+  (cond
+    ((null s) (error "s is nil"))
+    ((eq prop (first s)) (rplaca (cdr s) value))
+    ((null (cddr s)) (rplacd (cdr s) (list prop value)))
+    (t (set-property (cddr s) prop value))))
+
+(defun explode-pair (s)
+  (let* ((left (getf s :left))
+	 (right (getf s :right))
+	 (prev (getf left :prev))
+	 (next (getf right :next)))
+    (if prev
+	(incf (getf prev :value) (getf left :value)))
+    (if next
+	(incf (getf next :value) (getf right :value)))
+    ;; turn s into a leaf of value 0.
+    (set-property s :left nil)
+    (set-property s :right nil)
+    (set-property s :type 'leaf)
+    (set-property s :value 0)
+    (set-property s :prev prev)
+    (set-property s :next next)
+    (if prev (set-property prev :next s))
+    (if next (set-property next :prev s))))
+
+(defun split-value (s)
+  (let* ((value (getf s :value))
+	 (prev (getf s :prev))
+	 (next (getf s :next))
+	 (left (list :type 'leaf
+		     :value (floor (/ value 2))
+		     :depth (1+ (getf s :depth))))
+	 (right (list :type 'leaf
+		      :value (ceiling (/ value 2))
+		      :depth (1+ (getf s :depth)))))
+    ;; turn s into a pair.
+    (set-property s :value nil)
+    (set-property s :prev nil)
+    (set-property s :next nil)
+    (set-property s :type 'pair)
+    (set-property s :left left)
+    (set-property s :right right)
+    (if prev (set-property prev :next left))
+    (set-property left :next right)
+    (set-property right :next next)
+    (if next (set-property next :prev right))
+    (set-property right :prev left)
+    (set-property left :prev prev)))
+
+(defun reduce-snailfish-once (s)
+  "Performs up to 1 reduction on the input expression. Returns whether a reduction was performed."
+  (ecase (getf s :type)
+    (pair
+     ;; explode the pair if it is 4 levels deep
+     (if (>= (getf s :depth) 4)
+	 (progn (explode-pair s) t)
+	 (or (reduce-snailfish-once (getf s :left))
+	     (reduce-snailfish-once (getf s :right)))))
+    (leaf
+     (when (>= (getf s :value) 10)
+       (split-value s)
+       t))))
+
+(defun reduce-snailfish (s)
+  (when *debug*
+    (format t "~a~%" (snailfish-string s)))
+  (when (reduce-snailfish-once s)
+    (reduce-snailfish s)))
+
+
+;; (defun incf-depth (s)
+;;   (incf (getf s :depth))
+;;   (when (eq (getf s :type) 'pair)
+;;     (incf-depth (getf s :left))
+;;     (incf-depth (getf s :right))))
+;; 
+;; (defun snailfish-add (s1 s2)
+;;   (incf-depth s1)
+;;   (incf-depth s2)
+;;   (let ((s (list :type 'pair
+;; 		 :depth 0
+;; 		 :left s1
+;; 		 :right s2))
+;; 	(s1-rightmost (first (last (get-all-leaves s1))))
+;; 	(s2-leftmost (first (get-all-leaves s2))))
+;;     (assert (null (getf s1-rightmost :next)))
+;;     (assert (null (getf s2-leftmost :prev)))
+;;     (set-property s1-rightmost :next s2-leftmost)
+;;     (set-property s2-leftmost :prev s1-rightmost)
+;;     (reduce-snailfish s)
+;;     s))
+;; 
+;; (defun snailfish-sum (ss)
+;;   (reduce #'snailfish-add ss))
+
+;; (defun snailfish-magnitude (s)
+;;   (ecase (getf s :type)
+;;     (leaf (getf s :value))
+;;     (pair (+
+;; 	   (* 3 (snailfish-magnitude (getf s :left)))
+;; 	   (* 2 (snailfish-magnitude (getf s :right)))))))
+
+;; (assert (= 3488
+;; 	   (snailfish-magnitude
+;; 	    (snailfish-parse
+;; 	     "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]"))))
+
+(defun part1 (input)
+  (snailfish-magnitude (snailfish-sum input)))
+
+;; test things because I can't find the bug :-|
+(defun test-sum (text result)
+  (let ((successive-snailfishes
+	  (mapcar #'snailfish-parse
+		  (cl-utilities:split-sequence #\linefeed text))))
+    (loop with value-so-far = (first successive-snailfishes)
+	  for right-value in (rest successive-snailfishes)
+	  do (progn
+	       (format t "  ~a~%+ ~a~%"
+		       (snailfish-string value-so-far)
+		       (snailfish-string right-value))
+	       (let ((new-value (snailfish-add value-so-far right-value)))
+		 (format t "= ~a~%~%" (snailfish-string new-value))
+		 (setf value-so-far new-value)))
+	  finally (assert (string= (snailfish-string value-so-far) result)))))
+
+(test-sum
+ "[[[[4,3],4],4],[7,[[8,4],9]]]
+[1,1]"
+ "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")
+
+
+(test-sum
+ "[1,1]
+[2,2]
+[3,3]
+[4,4]"
+ "[[[[1,1],[2,2]],[3,3]],[4,4]]")
+
+(test-sum
+ "[1,1]
+[2,2]
+[3,3]
+[4,4]
+[5,5]"
+ "[[[[3,0],[5,3]],[4,4]],[5,5]]")
+
+(test-sum
+ "[1,1]
+[2,2]
+[3,3]
+[4,4]
+[5,5]
+[6,6]"
+ "[[[[5,0],[7,4]],[5,5]],[6,6]]")
+
+;; this fails
+(test-sum
+ "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]"
+ "[[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]]")
+;; mine:
+;;[[[[6,7],[7,7]],[[7,7],[7,0]]],[[[7,7],[7,8]],[[8,8],[8,9]]]]
+
+(defun assert-operation (left right expected-result)
+  (assert (string= (snailfish-string
+		    (snailfish-add
+		     (snailfish-parse left)
+		     (snailfish-parse right)))
+		   expected-result)))
+
+(defun test-add (lines)
+  (apply #'assert-operation
+	 (mapcar
+	  (lambda (line) (subseq line 2))
+	  (rest (cl-utilities:split-sequence #\linefeed lines)))))
+
+
+(test-add "
+  [[[[4,3],4],4],[7,[[8,4],9]]]
++ [1,1]
+= [[[[0,7],4],[[7,8],[6,0]]],[8,1]]")
+
+(let ((*debug* t))
+  (test-add "
+  [[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
++ [7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
+= [[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]"))
+;;mine:
+;;[[[[4,0],[5,4]],[[7,7],[6,0]]],[[[6,6],[5,6]],[[6,0],[7,7]]]]
+
+(test-add "
+  [[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]
++ [[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
+= [[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]")
+
+(test-add "
+  [[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]
++ [[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
+= [[[[7,0],[7,7]],[[7,7],[7,8]]],[[[7,7],[8,8]],[[7,7],[8,7]]]]")
+
+(test-add "
+  [[[[7,0],[7,7]],[[7,7],[7,8]]],[[[7,7],[8,8]],[[7,7],[8,7]]]]
++ [7,[5,[[3,8],[1,4]]]]
+= [[[[7,7],[7,8]],[[9,5],[8,7]]],[[[6,8],[0,8]],[[9,9],[9,0]]]]")
+
+(test-add "
+  [[[[7,7],[7,8]],[[9,5],[8,7]]],[[[6,8],[0,8]],[[9,9],[9,0]]]]
++ [[2,[2,2]],[8,[8,1]]]
+= [[[[6,6],[6,6]],[[6,0],[6,7]]],[[[7,7],[8,9]],[8,[8,1]]]]")
+
+(test-add "
+  [[[[6,6],[6,6]],[[6,0],[6,7]]],[[[7,7],[8,9]],[8,[8,1]]]]
++ [2,9]
+= [[[[6,6],[7,7]],[[0,7],[7,7]]],[[[5,5],[5,6]],9]]")
+
+(test-add "
+  [[[[6,6],[7,7]],[[0,7],[7,7]]],[[[5,5],[5,6]],9]]
++ [1,[[[9,3],9],[[9,0],[0,7]]]]
+= [[[[7,8],[6,7]],[[6,8],[0,8]]],[[[7,7],[5,0]],[[5,5],[5,6]]]]")
+
+(test-add "
+  [[[[7,8],[6,7]],[[6,8],[0,8]]],[[[7,7],[5,0]],[[5,5],[5,6]]]]
++ [[[5,[7,4]],7],1]
+= [[[[7,7],[7,7]],[[8,7],[8,7]]],[[[7,0],[7,7]],9]]")
+
+(test-add "
+  [[[[7,7],[7,7]],[[8,7],[8,7]]],[[[7,0],[7,7]],9]]
++ [[[[4,2],2],6],[8,7]]
+= [[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]")
---
+++ 19.lisp
@@ -0,0 +1,180 @@
+(ql:quickload 'cl-utilities)
+
+(defun parse (input)
+  (unless (null input)
+    (assert (string= (subseq (first input) 0 4) "--- "))
+    (loop for (line . rest) on (rest input)
+	  while (> (length line) 0)
+	  collect (mapcar #'parse-integer (cl-utilities:split-sequence #\, line)) into coords
+	  finally (return (cons coords (parse rest))))))
+
+(defparameter *input* (parse (uiop:read-file-lines "19.input")))
+(defparameter *input-test* (parse (uiop:read-file-lines "19.input.test")))
+
+(defun vec-op (op v1 v2)
+  (map 'list op v1 v2))
+
+(defun dot-product (a b)
+  (apply #'+ (vec-op #'* a b)))
+
+(defun cross-product (a b)
+  (destructuring-bind (ax ay az) a
+    (destructuring-bind (bx by bz) b
+      (list
+       (- (* ay bz) (* az by))
+       (- (* ax bz) (* az bx))
+       (- (* ax by) (* ay bx))))))
+
+;; angle is 1 2 or 3
+;; cos(0) = 1, cos(1) = 0, cos(2) = -1, cos(3) = 0
+;; sin(0) = 0, sin(1) = 1, sin(2) = 0, sin(3) = -1
+;; => sin(n) = cos(n-1)
+(defun my-cos (angle)
+  (ecase angle
+    (0 1)
+    (1 0)
+    (2 -1)
+    (3 0)))
+
+(defun my-sin (angle)
+  (my-cos (mod (1- angle) 4)))
+
+(defun rotate-vector (vector rotation-vector angle)
+  (map 'list #'+
+       (mapcar (lambda (v) (* v (my-cos angle))) vector)
+       (mapcar (lambda (v) (* v (my-sin angle)))
+	       (cross-product vector rotation-vector))
+       (mapcar (lambda (v) (* v (dot-product vector rotation-vector) (- 1 (my-cos angle))))
+	       rotation-vector)))
+
+(defparameter *unit-vectors*
+  '((1 0 0) (0 1 0) (0 0 1) (-1 0 0) (0 -1 0) (0 0 -1)))
+
+(defun is-null-vector (v)
+  (every #'zerop v))
+
+(defparameter *orientations*
+  ;; the first x can be in a 6 directions. For each of those, there is 4 possible y.
+  (loop for x in *unit-vectors*
+	nconc (loop for y in *unit-vectors*
+		    for z = (cross-product x y)
+		    unless (is-null-vector z)
+		      collect (list x y z))))
+
+(defun matmul (orientation vector)
+  (loop for axis in orientation
+	collect (dot-product axis vector)))
+
+(defun rotate-scanner-measure (measure orientation)
+  "Returns what measure would be if orientation were (1 0 0) (0 1 0) (0 0 1)."
+  ;; orientation can be seen as a matrix be which each measurement should be multiplied.
+  (mapcar (lambda (m) (matmul orientation m)) measure))
+
+(defun translate (m delta)
+  (loop for mm in m collect (vec-op #'+ mm delta)))
+
+(defun match-and-translate (m1 m2)
+  "Tries to match measurements in m1 with m2 (without rotating)."
+  ;; build a hash table of (delta vector) -> count. Stop when we found a delta that
+  ;; happens 12+ times.
+  ;; return m2 translated to match m1.
+  (loop with deltas = (make-hash-table :test #'equal)
+	for mm1 in m1 do
+	  (loop for mm2 in m2
+		for delta = (vec-op #'- mm1 mm2)
+		;; did we find at least 12 combinations of points with this delta?
+		if (>= (incf (gethash delta deltas 0)) 12)
+		  ;; yes. translate all of m2 with it.
+		  do (progn
+		       (format t "delta: ~a~%" delta)
+		       (return-from match-and-translate
+			 (loop for mm2 in m2
+			       collect (vec-op #'+ mm2 delta)))))))
+
+(defun match-and-translate2 (m1 m2)
+  "Tries to match measurements in m1 with m2 (without rotating)."
+  ;; for each of m1, for each of m2. Suppose they're the same. How many of m2 are then in m1?
+  (loop for mm1 in m1
+	do (loop for mm2 in m2
+		 for delta = (vec-op #'- mm1 mm2)
+		 if (>=
+		     (length
+		      (intersection
+		       (loop for mm22 in m2 collect (vec-op #'+ mm22 delta))
+		       m1 :test #'equal))
+		     12)
+		   do (progn
+			(format t "delta: ~a~%" delta)
+			(return-from match-and-translate2
+			  (loop for mm2 in m2
+				collect (vec-op #'+ mm2 delta)))))))
+
+(defun try-merge-measurement (measurement merged-measurements)
+  "Returns measurement rotated & translated in case of success)."
+  (loop for o in *orientations*
+	for rotated-m = (rotate-scanner-measure measurement o)
+	do (loop for mm in merged-measurements
+		 for translated-m = (match-and-translate mm rotated-m)
+		 if translated-m do
+		   (progn
+		     (format t "orientation ~a~%now starts with ~a~%~%"
+			     o (first translated-m))
+		     (return-from try-merge-measurement translated-m)))))
+
+(defun rot (v) (append (rest v) (list (first v))))
+
+(defun merge-measurements (measurements &optional merged-measurements global-map)
+  "Merges all measurements into global-map. merged-measurements is a list of
+   rotated-and-translated-measurement that were already merged into global-map."
+  (cond
+    ;; init
+    ((null merged-measurements)
+     (format t "start from ~a...~%" (first (first measurements)))
+     (merge-measurements (rest measurements)
+			 (list (first measurements))
+			 (first measurements)))
+    ;; end
+    ((null measurements) global-map)
+    ;; merging
+    (t
+     (format t "try to merge ~a...~%"
+	     (first (first measurements)))
+     (let ((new-m
+	     (try-merge-measurement (first measurements) merged-measurements)))
+       (if new-m
+	   ;; update map and recurse
+	   (merge-measurements
+	    (rest measurements)
+	    (cons new-m merged-measurements)
+	    (remove-duplicates (append global-map new-m)))
+	   ;; queue it for future processing
+	   (merge-measurements
+	    (append (rest measurements) (list (first measurements)))
+	    merged-measurements
+	    global-map))))))
+
+(defun part1 (&optional (input *input*))
+  (length (merge-measurements input)))
+
+
+(defparameter *modified-fifth*
+  (translate
+   (rotate-scanner-measure (fifth *input-test*) '((0 -1 0) (0 0 -1) (1 0 0)))
+   '(-20 -1133 1061)))
+
+;; works
+(print
+ (match-and-translate
+  (third *input-test*)
+  (rotate-scanner-measure (fifth *input-test*) '((0 1 0) (1 0 0) (0 0 -1)))))
+
+(defun compose-rotations (r1 r2)
+  )
+
+;; does not T_T
+;; yeah ofc I need to update the angle
+(loop for o in *orientations*
+      for translated = (match-and-translate (third *input-test*)
+					    (rotate-scanner-measure *modified-fifth* o))
+      when translated
+	do (format t "~a~%" translated))
