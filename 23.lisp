d21abf2801be7998eb7fe813a5de87d5a321db4a
Bruno Cauet <brunocauet@gmail.com>
2021-12-28 16:20:29 +0100

    Solve day 23.

--- 23.lisp
+++ 23.lisp
@@ -2,7 +2,7 @@
 
 ;; the world is a list
 ;; each item is either ('spot . contents)
-;; or (('entrance . room-owner) . (contents1 contents2))
+;; or (('entrance . room-owner) . contents-list)
 (defun make-world (rooms-contents)
   `((spot)
     (spot)
@@ -23,22 +23,17 @@
 		   (if (entrance-p tile)
 		       #\.
 		       (or (tile-contents tile) #\.))))
-  (format s "#~%#")
-  ;; first row of entrances
-  (loop for tile in world
-	do (format s "~a"
-		   (if (spot-p tile)
-		       #\#
-		       (or (first (tile-contents tile)) #\.))))
-  (format s "#~%  ")
-  ;; second row of entrances (skip 1 spot on each side)
-  (loop for (tile . rest) on (rest world)
-	while rest
-	do (format s "~a"
-		   (if (spot-p tile)
-		       #\#
-		       (or (second (tile-contents tile)) #\.))))
-  (format s "~%  #########~%"))
+  (format s "#~%")
+  ;; rows of entrances
+  (loop for row-count from 0 below (length (tile-contents (third world)))
+	do (loop for tile in world
+		   initially (format s " ")
+		 do (format s "~a"
+			    (if (spot-p tile)
+				#\#
+				(or (nth row-count (tile-contents tile)) #\.)))
+		 finally (format s "~%")))
+  (format s " ###########~%"))
 
 (defun draw-world (world)
   ;; inverse function of parse-world.
@@ -46,11 +41,11 @@
     (print-world world s)))
 
 (defun parse (input)
-  (let ((relevant-lines (subseq input 2 4)))
-    (make-world
-     (loop for index in '(3 5 7 9)
-	   collect (loop for line in relevant-lines
-			 collect (intern (string (char line index))))))))
+  (make-world
+   (loop for index in '(3 5 7 9)
+	 collect (loop for line in input
+		       if (some #'alphanumericp (coerce line 'list))
+			 collect (intern (string (char line index)))))))
 
 ;; inputs & goal
 (defparameter *input* (parse (uiop:read-file-lines "23.input")))
@@ -95,43 +90,59 @@
     (C 100)
     (D 1000)))
 
+(defun kick-out-next (contents &optional (depth 1))
+  "Returns (values depth victim new-situation)."
+  (unless contents
+    (error "Noone to kick out!"))
+  (if (first contents)
+      (values depth
+	      (first contents)
+	      (append (loop for d below depth collect nil)
+		      (rest contents)))
+      (kick-out-next (rest contents) (1+ depth))))
+(assert (equal '(2 a (nil nil b c))
+	       (multiple-value-list (kick-out-next '(nil a b c)))))
+
+(defun try-kick-out-of-room (owner contents)
+  "Returns (cost amphipod new-room)."
+  (unless (every (lambda (c) (or (null c) (eq owner c))) contents)
+    ;; kick out the first non-nil
+    (multiple-value-bind (depth out new-contents) (kick-out-next contents)
+      (list 
+       (* depth (move-cost out))
+       out
+       (make-room owner new-contents)))))
+
 (defun next-out (tile)
   "If an amphipod can move out of tile, returns (cost amphipod new-tile)."
   (if (spot-p tile)
       (list 0 (tile-contents tile) (make-spot))
-      (let* ((room-owner (entrance-p tile))
-	     (room-contents (tile-contents tile))
-	     (first-a (first room-contents))
-	     (second-a (second room-contents)))
-	(if first-a
-	    (unless (and (eq room-owner first-a)
-			 (eq room-owner second-a))
-	      ;; move out
-	      (list (move-cost first-a)
-		    first-a
-		    (make-room room-owner (list nil second-a))))
-	    (if second-a
-		(unless (eq room-owner second-a)
-		  ;; move out
-		  (list (* 2 (move-cost second-a))
-			second-a
-			(make-room room-owner))))))))
+      (try-kick-out-of-room (entrance-p tile)
+			    (tile-contents tile))))
+
+(defun get-into-room (contents in &optional (depth 1))
+  "Returns nil if not possible, or (cost . new-contents)"
+  (if (null contents)
+      nil
+      (let ((rec (get-into-room (rest contents) in (1+ depth))))
+	(if rec
+	    (cons (car rec)
+		  (cons (first contents) (cdr rec)))
+	    (if (null (first contents))
+		;; insert here
+		(cons (* depth (move-cost in))
+		      (cons in (rest contents)))
+		nil)))))
 
 (defun enter-room (tile amphipod)
   "Returns (cost . new-room) when amphipod enters tile if it can, or nil"
   (assert (eq amphipod (entrance-p tile)))
-  (cond
-    ;; intruders in the room
-    ((some (lambda (a) (and a (not (eq a amphipod)))) (tile-contents tile))
-     nil)
-    ;; noone in it
-    ((null (second (tile-contents tile)))
-     (cons (* 2 (move-cost amphipod))
-	   (make-room amphipod (list nil amphipod))))
-    ;; first spot is still free
-    ((null (first (tile-contents tile)))
-     (cons (move-cost amphipod)
-	   (make-room amphipod (cons amphipod (rest (tile-contents tile))))))))
+  (if (some (lambda (a) (and a (not (eq a amphipod)))) (tile-contents tile))
+      ;; intruders in the room
+      nil
+      (destructuring-bind (cost . new-contents)
+	  (get-into-room (tile-contents tile) amphipod)
+	(cons cost (make-room amphipod new-contents)))))
 
 (defun try-enter (current-tile-type tile amphipod)
   "Try to move amphipod into tile, returning (cost . new-tile), or nil."
@@ -181,19 +192,32 @@
 					(cons tile-out world-right)))))
 	collect tile into past))
 
-(defun solve-world (world)
-  "Returns a list of [(cost . goal)]."
-  (loop for cost-and-new-world in (possible-moves world)
-	for (cost . new-world) = cost-and-new-world
-	nconc (if (equal world *goal*)
-		    (progn
-		      (format t "found a solution at cost ~a~%" cost)
-		      (list cost-and-new-world))
-		    ;; recurse
-		    (loop for (cost2 . new-world2) in (solve-world new-world)
-			  if new-world2
-			    collect (cons (+ cost cost2) new-world2)))))
+(defun paths (from to cost visited)
+  (if (equal from to)
+      (cons cost (reverse visited))
+      (reduce (lambda (c-p-1 c-p-2)
+		(if (< (car c-p-1) (car c-p-2) ) c-p-1 c-p-2))
+	      (loop for (c . next) in (possible-moves from)
+		    unless (member next visited :test #'equal)
+		      collect (paths next to (+ c cost) (cons next visited)))
+	      :initial-value (list most-positive-fixnum))))
 
-(defun cheap-solve (initial-world)
-  (loop for (cost . goal-world) in (solve-world initial-world)
-	minimize cost))
+(defun dijkstra-short-path (start end)
+  (paths start end 0 (list start)))
+
+(defun part1 (&optional (input *input*) (goal *goal*))
+  (destructuring-bind (cost . steps) (dijkstra-short-path input goal)
+    (format t "cost: ~a.~%" cost)
+    (loop for s in steps
+	  do (print-world s)
+	  do (terpri))
+    cost))
+
+(defparameter *input2* (parse (uiop:read-file-lines "23.input.2")))
+(defparameter *input-test2* (parse (uiop:read-file-lines "23.input.test.2")))
+(defparameter *goal2*
+  (make-world '((A A A A) (B B B B) (C C C C) (D D D D))))
+(defun test2 ()
+  (assert (= 44169 (part1 *input-test2* *goal2*))))
+
+(defun part2 () (part1 *input2* *goal2*))
