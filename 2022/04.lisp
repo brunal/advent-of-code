71e794f2491acda392b15aa19e1a26fbf651a7a4
Bruno Cauet <brunocauet@gmail.com>
2022-12-05 15:38:14 +0100

    Simplify 04.lisp.

--- 2022/04.lisp
+++ 2022/04.lisp
@@ -1,12 +1,9 @@
-(ql:quickload "cl-utilities")
+(require 'uiop)
 
 (defun parse-lines (lines)
   (mapcar
    (lambda (line)
-     (mapcar (lambda (pair)
-	       (mapcar (lambda (number) (parse-integer number))
-		       (cl-utilities:split-sequence #\- pair)))
-	     (cl-utilities:split-sequence #\, line)))
+     (mapcar #'parse-integer (uiop:split-string line :separator "-,")))
    lines))
 
 (defparameter *input* (parse-lines (uiop:read-file-lines "04.input")))
@@ -15,31 +12,21 @@
   "Returns t if [x1, x2] fully contains [y1, y2]"
   (and (<= x1 y1) (>= x2 y2)))
 
-(defun range-fully-contains (x y)
-  (let ((x1 (car x))
-	(x2 (cadr x))
-	(y1 (car y))
-	(y2 (cadr y)))
-    (or (left-contains-right x1 x2 y1 y2)
-	(left-contains-right y1 y2 x1 x2))))
+(defun range-fully-contains (x1 x2 y1 y2)
+  (or (left-contains-right x1 x2 y1 y2)
+      (left-contains-right y1 y2 x1 x2)))
 
 (defun count-pairs-that (input predicate)
-  (reduce #'+
-	  (loop for pair in input
-		when (apply predicate pair)
-		  collect 1)))
+  (loop for pair in input
+	counting (apply predicate pair)))
 
 (defun part1 (input)
   (count-pairs-that input #'range-fully-contains))
 
-(defun range-overlaps (x y)
-  (let ((x1 (car x))
-	(x2 (cadr x))
-	(y1 (car y))
-	(y2 (cadr y)))
-    (or (and (<= x1 y1) (>= x2 y1))
-	(and (<= x1 y2) (>= x2 y2))
-	(and (>= x1 y1) (<= x2 y2)))))
+(defun range-overlaps (x1 x2 y1 y2)
+  (or (and (<= x1 y1) (>= x2 y1))
+      (and (<= x1 y2) (>= x2 y2))
+      (and (>= x1 y1) (<= x2 y2))))
 
 (defun part2 (input)
   (count-pairs-that input #'range-overlaps))
