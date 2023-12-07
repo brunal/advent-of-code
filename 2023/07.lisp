49ee2c218a5b09fda8ab59e71ef26d685dd8951e
Bruno Cauet <brunocauet@gmail.com>
2023-12-07 10:46:58 +0100

    Cleanup 07.lisp.

--- 2023/07.lisp
+++ 2023/07.lisp
@@ -8,24 +8,31 @@ KTJJT 220
 QQQJA 483" :separator (string #\newline)))
 
 (defun parse-input (input)
+  "Returns a list of (hand . value)."
   (loop for line in input
 	for (hand value) = (uiop:split-string line :separator (string #\Space))
 	collect (cons (parse-hand hand) (parse-integer value))))
 
+(defun parse-hand (hand)
+  "Returns a 5-list of card values (elements of *card-order*)."
+  (mapcar (lambda (char) (or (digit-char-p char) (intern (string char))))
+	  (coerce hand 'list)))
+
 (defparameter *card-order* '(2 3 4 5 6 7 8 9 T J Q K A))
 
 (defun count-most-frequent-card (hand)
-  (let* ((ht (make-hash-table))
-	 (max-count (loop for card in hand maximizing (incf (gethash card ht 0))))
-	 (second-max-count (second (sort (loop for v being the hash-values of ht collect v) #'>))))
-    ;; hack: handle double pairs, as well as full house
-    (if (and second-max-count (= 2 second-max-count))
-	(+ max-count 0.5)
-	max-count)))
-
-(defun parse-hand (hand)
-  (mapcar (lambda (char) (or (digit-char-p char) (intern (string char))))
-	  (coerce hand 'list)))
+  "Returns the frequency of the most frequent card, +.5 if there is a pair in the other cards."
+  (let ((ht (make-hash-table)))
+    (loop for card in hand do (incf (gethash card ht 0)))
+    (let* ((frequencies (sort (loop for v being the hash-values of ht collect v) #'>))
+	   (max-count (first frequencies))
+	   (second-max-count (second frequencies)))
+      ;; handle double pairs, as well as full house
+      (cond
+	((and second-max-count (= 2 second-max-count)) (+ max-count 0.5))
+	(max-count max-count)
+	;; empty hand happens in part2 when we got '(J J J J J).
+	(t 0)))))
 
 (defun hands<= (h1 h2 count-freq card-order)
   (let ((f1 (funcall count-freq h1))
@@ -44,17 +51,21 @@ QQQJA 483" :separator (string #\newline)))
 		       finally
 			  (return t))))))
 
-(defun parse-and-sort-input (input &optional
-				     (count-freq #'count-most-frequent-card)
-				     (card-order *card-order*))
+(defun parse-and-sort-input (input count-freq card-order)
   (sort (parse-input input)
 	(lambda (h1 h2) (hands<= h1 h2 count-freq card-order))
 	:key #'first))
 
-(defun part1 (input)
-  (loop for (hand . value) in (parse-and-sort-input input)
+(defun hands-value (sorted-hands)
+  (loop for (hand . value) in sorted-hands
 	for i from 1
 	sum (* i value)))
+
+(defun part1 (input)
+  (hands-value
+   (parse-and-sort-input input
+			 #'count-most-frequent-card
+			 *card-order*)))
 
 (print (part1 *input*))
 
@@ -62,20 +73,17 @@ QQQJA 483" :separator (string #\newline)))
 
 (defun count-most-frequent-card-part2 (hand)
   ;; Fist remove J, then add them to the most frequent card.
-  (multiple-value-bind (j-count hand-no-j)
-      (loop for card in hand
-	    if (eq card 'J)
-	      sum 1 into j-count
-	    else
-	      collect card into hand-no-j
-	    finally (return (values j-count hand-no-j)))
-    (+ j-count (count-most-frequent-card hand-no-j))))
+  (loop for card in hand
+	if (eq card 'J)
+	  sum 1 into j-count
+	else
+	  collect card into hand-no-j
+	finally (return (+ j-count (count-most-frequent-card hand-no-j)))))
 
 (defun part2 (input)
-  (loop for (hand . value) in (parse-and-sort-input input
-						    #'count-most-frequent-card-part2
-						    *card-order-part2*)
-	for i from 1
-	sum (* i value)))
+  (hands-value
+   (parse-and-sort-input input
+			 #'count-most-frequent-card-part2
+			 *card-order-part2*)))
 
 (print (part2 *input*))
